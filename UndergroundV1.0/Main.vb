222bae4ce05f5cdd35856fab35d3035ec8a3df79
danny <danny@DESKTOP-009BUV3>
2022-12-20 13:59:48 +0000

    Add project files.

---
+++ UndergroundV1.0/Main.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.Reflection.Assembly
+Imports System.Reflection
+
+Module Main
+    Sub Main()
+        Dim bf As New BinaryFormatter
+        Console.WriteLine("Choose a Station")
+        Dim Station = Console.ReadLine()
+        Using ms As New MemoryStream(My.Resources.ACTON_TOWN)
+            For Each l In bf.Deserialize(ms)
+                Console.WriteLine(l)
+            Next
+        End Using
+    End Sub
+End Module
