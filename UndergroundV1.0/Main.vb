194ca564eee41d91268ac103bdece83b9bdf14f6
danny <danny@DESKTOP-009BUV3>
2022-12-21 15:18:55 +0000

    Dijkstras fully functional, need to serialize and store results in resources

--- UndergroundV1.0/Main.vb
+++ UndergroundV1.0/Main.vb
@@ -1,34 +1,122 @@
 ﻿Imports System.IO
+Imports System.Runtime.InteropServices.WindowsRuntime
 Imports System.Runtime.Serialization.Formatters.Binary
-Imports System.Reflection.Assembly
-Imports System.Reflection
-
 Module Main
 
+    Private Stations(271) As Station
+
     Sub Main()
+        initStations()
+        initConnections()
+        getShortestRoutes()
     End Sub
-    Sub shortestRoute()
-        Console.WriteLine("Enter a Start Station: ")
-        Dim Start = Console.ReadLine
-        Console.WriteLine("Enter a Finish Station")
-        Dim Finish = Console.ReadLine
-        Dim ShortestRoutes As Dictionary(Of Integer, DijkNode) = Deserialize(Start.Replace(" ", "_").ToUpper)
-        Console.WriteLine("The shortest distance from " & Start & " to " & Finish & " is " & ShortestRoutes.First(Function(x) x.Value.Name.Replace("_", " ").ToUpper = Finish.ToUpper).Value.Distance & "km")
+    Sub initStations()
+        Using reader As TextReader = New StringReader(My.Resources.StationNames)
+            Dim i = 0
+            Do Until i = 272
+                Stations(i) = New Station(reader.ReadLine)
+                i += 1
+            Loop
+        End Using
     End Sub
 
-    Function Deserialize(Name As String) As Dictionary(Of Integer, DijkNode)
-        Using ms = New MemoryStream(CType(My.Resources.ResourceManager.GetObject(Name), Byte()))
-            Dim bf As New BinaryFormatter()
-            Return bf.Deserialize(ms)
+    Sub initConnections()
+        Using reader As TextReader = New StringReader(My.Resources.StationConnections)
+            Dim i = 0
+            Do Until i = 743
+                Dim Info As List(Of String) = reader.ReadLine.Split(",").ToList
+                Stations.First(Function(Start) Info(2) = Start.GetName).AddConnection(Info)
+                i += 1
+            Loop
         End Using
+    End Sub
+
+    Sub getShortestRoutes()
+        For i = 0 To 271
+            For j = 3 To 6
+                For Each Station In Dijkstras(Stations(i).GetName, j)
+                    Stations(i).addShortestRoute(Station, j - 2)
+                Next
+            Next
+            Console.WriteLine(i)
+            Stations(i).TempCost = 0
+            Stations(i).TempParent = 0
+        Next
+    End Sub
+
+
+    Function Dijkstras(Start As String, MinimumIndex As Integer) As List(Of Station)
+        Dim Visited As New List(Of Station)
+        Dim Unvisited As List(Of Station) = Stations.ToList
+        Dim CurrentStation As Station
+        Dim AdjacentStation As Station
+
+        For Each S In Stations
+            S.TempCost = 1000
+        Next
+        Unvisited(Unvisited.FindIndex(Function(Station) Station.GetName = Start.ToUpper)).TempCost = 0
+        Do
+            CurrentStation = Unvisited.OrderBy(Function(Station) Station.TempCost).First
+            For Each Connection In CurrentStation.GetInfo
+                If Not Visited.Contains(Stations.First(Function(Station) Station.GetName = Connection(2))) Then
+                    AdjacentStation = Unvisited(Unvisited.FindIndex(Function(Station) Station.GetName = Connection(2)))
+                Else
+                    Continue For
+                End If
+                If AdjacentStation.TempCost = 1000 Or AdjacentStation.TempParent = Array.IndexOf(Stations, CurrentStation) Then
+                    If AdjacentStation.TempCost > Connection(MinimumIndex) Then AdjacentStation.TempCost = Connection(MinimumIndex) + CurrentStation.TempCost
+                Else
+                    If AdjacentStation.TempCost > CurrentStation.TempCost + Connection(MinimumIndex) Then AdjacentStation.TempCost += Connection(MinimumIndex)
+                End If
+                AdjacentStation.TempParent = Array.IndexOf(Stations, CurrentStation)
+            Next
+            Unvisited.Remove(CurrentStation)
+            Visited.Add(CurrentStation)
+        Loop Until Unvisited.Count = 0
+        Return Visited
     End Function
-    <Serializable> Class DijkNode ' structure used to store stations for use in dijkstras algorithm
-        Public Name As String
-        Public Cost As Decimal
-        Public Distance As Decimal
-        Public IdealTime As Decimal
-        Public PeakTime As Decimal
-        Public OffPeakTime As Decimal
-        Public Prev As Integer
+
+    Class Station
+        Private Name As String
+        Private ShortestRoutes As New List(Of Tuple(Of String, Integer)())
+        Private Adjacents As New List(Of List(Of String))
+        'each entry is an adjacent station and all info about that station in the form:
+        'Line, Direction, Adjacent Station, Distance(km), Ideal Time(mins), Peak Time(mins), Off Peak Time(mins)
+        Public TempCost As Decimal
+        Public TempParent As Integer
+
+
+        Sub New(NewName As String)
+            Name = NewName
+        End Sub
+
+        Sub AddConnection(Info As List(Of String))
+            Info.RemoveAt(2)
+            Adjacents.Add(Info)
+        End Sub
+
+        Sub addShortestRoute(Station As Station, Index As Integer)
+            If ShortestRoutes.Find(Function(S) S(0).Item1 = Station.GetName) Is Nothing Then
+                Dim NewRoute(4) As Tuple(Of String, Integer)
+                NewRoute(0) = New Tuple(Of String, Integer)(Station.GetName, -1)
+                NewRoute(Index) = New Tuple(Of String, Integer)(Station.TempCost, Station.TempParent)
+                ShortestRoutes.Add(NewRoute)
+                Exit Sub
+            Else
+                ShortestRoutes.Find(Function(S) S(0).Item1 = Station.GetName)(Index) = New Tuple(Of String, Integer)(Station.TempCost, Station.TempParent)
+            End If
+        End Sub
+
+        ReadOnly Property GetName As String
+            Get
+                Return Name
+            End Get
+        End Property
+
+        ReadOnly Property GetInfo As List(Of List(Of String))
+            Get
+                Return Adjacents
+            End Get
+        End Property
     End Class
 End Module
